397b82c90a3616984f430121fac43f2052dddbe1
kingswhale <hardeyreemyarfyc@gmail.com>
2018-04-06 13:23:31 +0100

    Initial commit

---
+++ app/src/main/java/ng/nhub/deliveryapp/Activity/SignUp.java
@@ -0,0 +1,28 @@
+package ng.nhub.deliveryapp.Activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import ng.nhub.deliveryapp.R;
+
+public class SignUp extends AppCompatActivity {
+
+    Button btnSignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(SignUp.this, Home.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        });
+    }
+}
